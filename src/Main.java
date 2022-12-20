d7fe5136e6a9005a1f761d9b7de316968ef524e3
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-12-20 09:02:10 -0300

    feat: reading values method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //read txt file - separate each line into two different lists of 5 cards - new Card() separated by blank spaces
+        //read txt file - separate each line into two different lists of 5 cards - new Card(value, suit) separated by blank spaces
+        ReadTxtFile readTxtFile = new ReadTxtFile();
+        List<List<Card>> firstAndSecondHandCards = readTxtFile.execute();
 
         //identify ranks inside first and second list per line
+            //identifyRank(List(5)) -> returns the rank, the highest card of the deck (based on value and suit)
+                //returns the respective Rank Object -> isHighCard(5), isOnePair(5), and so on
+                //Rank Object -> HighCard(List<Card> cards, Card highestCard, rank(1))
+
 
         //compare ranks and define which one is the higher -> first++ or second++;
+            //if "rank" fields are equal, should compare the "highestCard" field
+            //if both hands are equal it's a tie
+
     }
 }
---
+++ src/ReadTxtFile.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadTxtFile {
+
+    public ReadTxtFile() {}
+
+    public List<List<Card>> execute() {
+
+        List<Card> firstHandCards = new ArrayList<>();
+        List<Card> secondHandCards = new ArrayList<>();
+
+        try {
+
+            URL url = new URL("https://projecteuler.net/project/resources/p054_poker.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = bufferedReader.readLine();
+
+            //CHANGE NULL CHECK
+            while (line != null) {
+                String cards[] = line.split(" ");
+
+                int i = 0;
+                for(String card: cards) {
+                    Card cardObj = new Card(String.valueOf(card.charAt(0)), String.valueOf(card.charAt(1)));
+
+                    if (i < 5) {
+                        firstHandCards.add(cardObj);
+                        i++;
+                    } else {
+                        secondHandCards.add(cardObj);
+                    }
+                }
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return List.of(firstHandCards, secondHandCards);
+    }
+
+}
