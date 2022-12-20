c3390f43b046e4f3bd3582cf54d1f86d25be485b
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-12-20 07:22:27 -0300

    feat: project created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //read txt file - separate each line into two different lists of 5 cards - new Card() separated by blank spaces
+
+        //identify ranks inside first and second list per line
+
+        //compare ranks and define which one is the higher -> first++ or second++;
+    }
+}
