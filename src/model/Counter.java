86d406db2965c5fb3f767577484a22f6a920d8af
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-12-26 10:53:26 -0300

    feat: Counter class -> represents each hand's points per line

---
+++ src/model/Counter.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Counter {
+
+    private Integer firstCounter;
+    private Integer secondCounter;
+
+    public Counter(Integer firstCounter, Integer secondCounter) {
+        this.firstCounter = firstCounter;
+        this.secondCounter = secondCounter;
+    }
+
+    public Counter() {}
+
+    public Integer getFirstCounter() {
+        return firstCounter;
+    }
+
+    public void setFirstCounter(Integer firstCounter) {
+        this.firstCounter = firstCounter;
+    }
+
+    public Integer getSecondCounter() {
+        return secondCounter;
+    }
+
+    public void setSecondCounter(Integer secondCounter) {
+        this.secondCounter = secondCounter;
+    }
+}
