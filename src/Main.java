383b4a6aabc0c3ccc5ab0f6997d4133c8661a1cf
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-12-26 11:28:16 -0300

    chore: changing Main class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-import component.DefineWinner;
-import component.ReadTxtFile;
-import model.Card;
-
-import java.util.List;
+import component.DefineHandCounts;
 
 public class Main {
     public static void main(String[] args) {
-        //read txt file - separate each line into two different lists of 5 cards - new model.Card(value, suit) separated by blank spaces
-        ReadTxtFile readTxtFile = new ReadTxtFile();
-        List<List<Card>> firstAndSecondHandCards = readTxtFile.execute();
-
-        //identify ranks of each deck and comes up with winner
-        DefineWinner defineWinner = new DefineWinner();
-        String winnerValue = defineWinner.execute(firstAndSecondHandCards.get(0), firstAndSecondHandCards.get(1));
-
-        System.out.println(winnerValue);
-
+        DefineHandCounts defineHandCounts = new DefineHandCounts();
+        String winner = defineHandCounts.execute();
+        System.out.println(winner);
     }
 }
